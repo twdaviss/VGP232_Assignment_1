b6584220777022f2090aa074b5bdc54450862608
Taylor Daviss <twdaviss99@gmail.com>
2022-10-12 20:33:28 -0700

    Add project files.

---
+++ VGP232_Assignment_1/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+// TODO: Fill in your name and student number.
+// Assignment 1
+// NAME: 
+// STUDENT NUMBER: 
+
+namespace Assignment1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Variables and flags
+
+            // The path to the input file to load.
+            string inputFile = string.Empty;
+
+            // The path of the output file to save.
+            string outputFile = string.Empty;
+
+            // The flag to determine if we overwrite the output file or append to it.
+            bool appendToFile = false;
+
+            // The flag to determine if we need to display the number of entries
+            bool displayCount = false;
+
+            // The flag to determine if we need to sort the results via name.
+            bool sortEnabled = false;
+            
+            // The column name to be used to determine which sort comparison function to use.
+            string sortColumnName = string.Empty;
+
+            // The results to be output to a file or to the console
+            List<Weapon> results = new List<Weapon>();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                // h or --help for help to output the instructions on how to use it
+                if (args[i] == "-h" || args[i] == "--help")
+                {
+                    Console.WriteLine("-i <path> or --input <path> : loads the input file path specified (required)");
+                    Console.WriteLine("-o <path> or --output <path> : saves result in the output file path specified (optional)");
+                    Console.WriteLine("-c or --count : displays the number of entries in the input file (optional).");
+                    Console.WriteLine("-a or --append : enables append mode when writing to an existing output file (optional)");
+                    Console.WriteLine("-s or --sort <column name> : outputs the results sorted by column name");
+
+
+                    // TODO: include help info for count
+                    //"-c or --count : displays the number of entries in the input file (optional).";
+
+                    // TODO: include help info for append
+                    //"-a or --append : enables append mode when writing to an existing output file (optional)";
+
+                    // TODO: include help info for sort
+                    //"-s or --sort <column name> : outputs the results sorted by column name";
+
+                    break;
+                }
+                else if (args[i] == "-i" || args[i] == "--input")
+                {
+                    // Check to make sure there's a second argument for the file name.
+                    if (args.Length > i + 1)
+                    {
+                        // stores the file name in the next argument to inputFile
+                        ++i;
+                        inputFile = args[i];
+
+                        if (string.IsNullOrEmpty(inputFile))
+                        {
+                            // TODO: print no input file specified.
+                            Console.WriteLine("No Input File Specified");
+                        }
+                        else if (!File.Exists(inputFile))
+                        {
+                            // TODO: print the file specified does not exist.
+                            Console.WriteLine("Specified File Does Not Exist");
+
+                        }
+                        else
+                        {
+                            // This function returns a List<Weapon> once the data is parsed.
+                            results = Parse(inputFile);
+                        }
+                    }
+                }
+                else if (args[i] == "-s" || args[i] == "--sort")
+                {
+                    // TODO: set the sortEnabled flag and see if the next argument is set for the column name
+                    sortEnabled = true;
+                    // TODO: set the sortColumnName string used for determining if there's another sort function.
+                    if (args.Length > i + 1)
+                    {
+                        // stores the file name in the next argument to inputFile
+                        ++i;
+                        sortColumnName = args[i];
+                    }
+                }
+                else if (args[i] == "-c" || args[i] == "--count")
+                {
+                    displayCount = true;
+                }
+                else if (args[i] == "-a" || args[i] == "--append")
+                {
+                    // TODO: set the appendToFile flag
+                    appendToFile = true;
+                }
+                else if (args[i] == "-o" || args[i] == "--output")
+                {
+                    // validation to make sure we do have an argument after the flag
+                    if (args.Length > i + 1)
+                    {
+                        // increment the index.
+                        ++i;
+                        string filePath = args[i];
+                        if (string.IsNullOrEmpty(filePath))
+                        {
+                            // TODO: print No output file specified.
+                            Console.WriteLine("No Input File Specified");
+
+                        }
+                        else
+                        {
+                            // TODO: set the output file to the outputFile
+                            outputFile = "../output.csv";
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The argument Arg[{0}] = [{1}] is invalid", i, args[i]);
+                }
+            }
+
+            if (sortEnabled)
+            {
+                // TODO: add implementation to determine the column name to trigger a different sort. (Hint: column names are the 4 properties of the weapon class)
+                if (sortColumnName == "Name")
+                {
+                    results.Sort(Weapon.CompareByName);
+                }
+                else if (sortColumnName == "Type")
+                {
+                    results.Sort(Weapon.CompareByType);
+
+                }
+                else if (sortColumnName == "Rarity")
+                {
+                    results.Sort(Weapon.CompareByType);
+                }
+                else
+                {
+                    results.Sort(Weapon.CompareByAttack);
+                }
+                // print: Sorting by <column name> e.g. BaseAttack
+
+                // Sorts the list based off of the Weapon name.
+
+            }
+
+            if (displayCount)
+            {
+                Console.WriteLine("There are {0} entries", results.Count);
+            }
+
+            if (results.Count > 0)
+            {
+                if (!string.IsNullOrEmpty(outputFile))
+                {
+                    FileStream fs;
+
+                    // Check if the append flag is set, and if so, then open the file in append mode; otherwise, create the file to write.
+                    if (appendToFile && File.Exists((outputFile)))
+                    {
+                        fs = File.Open(outputFile, FileMode.Append);
+                    }
+                    else
+                    {
+                        fs = File.Open(outputFile, FileMode.Create);
+                    }
+
+                    // opens a stream writer with the file handle to write to the output file.
+                    using (StreamWriter writer = new StreamWriter(fs))
+                    {
+                        // Hint: use writer.WriteLine
+                        // TODO: write the header of the output "Name,Type,Rarity,BaseAttack"
+                        writer.WriteLine("Name,Type,Rarity,BaseAttack");
+                        // TODO: use the writer to output the results.
+                        for (int i = 0; i < results.Count; i++)
+                        {
+                            writer.WriteLine(results[i].ToString());
+                        }
+                        // TODO: print out the file has been saved.
+                        Console.WriteLine("File Saved");
+                    }
+                }
+                else
+                {
+                    // prints out each entry in the weapon list results.
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        Console.WriteLine(results[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Done!");
+        }
+
+        /// <summary>
+        /// Reads the file and line by line parses the data into a List of Weapons
+        /// </summary>
+        /// <param name="fileName">The path to the file</param>
+        /// <returns>The list of Weapons</returns>
+        public static List<Weapon> Parse(string fileName)
+        {
+            // TODO: implement the streamreader that reads the file and appends each line to the list
+            // note that the result that you get from using read is a string, and needs to be parsed 
+            // to an int for certain fields i.e. HP, Attack, etc.
+            // i.e. int.Parse() and if the results cannot be parsed it will throw an exception
+            // or can use int.TryParse() 
+
+            // streamreader https://msdn.microsoft.com/en-us/library/system.io.streamreader(v=vs.110).aspx
+            // Use string split https://msdn.microsoft.com/en-us/library/system.string.split(v=vs.110).aspx
+
+            List<Weapon> output = new List<Weapon>();
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                // Skip the first line because header does not need to be parsed.
+                // Name,Type,Rarity,BaseAttack
+
+                string header = reader.ReadLine();
+
+                // The rest of the lines looks like the following:
+                // Skyward Blade,Sword,5,46
+                while (reader.Peek() > 0)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+
+                    Weapon weapon = new Weapon();
+
+                    if (values.Length == 4)
+                    {
+                        weapon.Name = values[0];
+                        weapon.Type = values[1];
+                        weapon.Rarity = int.Parse(values[2]);
+                        weapon.BaseAttack = int.Parse(values[3]);
+                    }
+                    output.Add(weapon);
+                    // TODO: validate that the string array the size expected.
+                    // TODO: use int.Parse or TryParse for stats/number values.
+                    // Populate the properties of the Weapon
+                    // TODO: Add the Weapon to the list
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ VGP232_Assignment_1/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Weapon
+    {
+        // Name,Type,Rarity,BaseAttack
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Rarity { get; set; }
+        public int BaseAttack { get; set; }
+        
+        /// <summary>
+        /// The Comparator function to check for name
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        public static int CompareByName(Weapon left, Weapon right)
+        {
+            return left.Name.CompareTo(right.Name);
+        }
+
+        // TODO: add sort for each property:
+        // CompareByType
+        public static int CompareByType(Weapon left, Weapon right)
+        {
+            return left.Type.CompareTo(right.Type);
+        }
+        // CompareByRarity\
+        public static int CompareByRarity(Weapon left, Weapon right)
+        {
+            return left.Rarity.CompareTo(right.Rarity);
+        }
+        // CompareByBaseAttack
+        public static int CompareByAttack(Weapon left, Weapon right)
+        {
+            return left.BaseAttack.CompareTo(right.BaseAttack);
+        }
+
+        /// <summary>
+        /// The Weapon string with all the properties
+        /// </summary>
+        /// <returns>The Weapon formated string</returns>
+        public override string ToString()
+        {
+            // TODO: construct a comma seperated value string
+            // Name,Type,Rarity,BaseAttack
+            return $"{Name},{Type},{Rarity},{BaseAttack}";
+        }
+    }
+}
